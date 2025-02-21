1c2c06ce1e4b648b2399021978b0de69691e137d
PujoPrayogo <pujoprayogo2@gmail.com>
2025-02-21 10:32:35 +0700

    Update 2_🔢_Data.py

--- pages/2_🔢_Data.py
+++ pages/2_🔢_Data.py
@@ -50,6 +50,26 @@ metrics = ['0/1', 'm/s', 'kPa', 'mm/hari', '%', 'C']
 
 df = st.session_state["df_all"]
 
+features = [
+    "GWETROOT", 
+    "WS10M", 
+    "PS", 
+    "PRECTOTCORR",
+    "RH2M", 
+    "T2M"]
+
+with st.expander("Parameter Cuaca"):
+    st.markdown(
+    """
+    - 🌱:rainbow-background[Kelembaban Tanah (0-1)] : Jumlah air dan uap air yang tersedia bagi tanaman di zona perakaran, yang umumnya dianggap sebagai lapisan tanah hingga kedalaman 200 cm, dinyatakan sebagai proporsi air yang terdapat dalam sejumlah tanah tertentu. Nilainya berkisar dari 0 untuk kondisi yang benar-benar kering hingga 1 untuk tanah yang benar-benar jenuh air.
+    - 💨:rainbow-background[Kecepatan Angin (m/s)] : Rata-rata kecepatan angin pada ketinggian 10 meter di atas permukaan bumi.
+    - 🌍:rainbow-background[Tekanan Permukaan Tanah (kPa)]: Tekanan atmosfer di permukaan bumi.
+    - 🌦️:rainbow-background[Presipitasi (mm/hari)] : Rata-rata curah hujan total yang telah dikoreksi bias MERRA-2 di permukaan bumi.
+    - 😤:rainbow-background[Kelembaban Udara Relatif (%)] : Rasio tekanan uap terhadap tekanan uap jenuh terhadap permukaan datar air murni, dinyatakan dalam persen.
+    - 🌡️:rainbow-background[Temperatur (°C)] : Rasio tekanan uap terhadap tekanan uap jenuh terhadap permukaan datar air murni, dinyatakan dalam persen.
+    """
+    )
+
 st.write(f'## Total Data: {len(df.columns) * len(df.index)}')
 st.write(f'Baris: {len(df)}')
 
@@ -86,12 +106,7 @@ for i, (column, metric) in enumerate(zip(df.columns, metrics), 1):
 plt.tight_layout()
 st.pyplot(plt.gcf())
 
-
-st.divider()
-st.markdown("## Autokorelasi")
-max_lags = st.slider("Jumlah lags", 0, 100, 50)
-
-# Fungsi untuk menghitung autokorelasi dan menentukan timesteps untuk semua parameter
+# ----- Fungsi Autokorelasi
 def determine_timesteps(df, max_lag):
     timesteps_dict = {}
     
@@ -109,6 +124,8 @@ def determine_timesteps(df, max_lag):
         st.pyplot(plt.gcf())
     return timesteps_dict
 
+with st.expander("Autokorelasi"):
+    max_lags = st.slider("Jumlah lags", 0, 100, 50)
 
-# Menjalankan fungsi untuk semua parameter dalam DataFrame
-timesteps_dict = determine_timesteps(df, max_lags)
+    # Menjalankan fungsi untuk semua parameter dalam DataFrame
+    timesteps_dict = determine_timesteps(df, max_lags)
